a13c89d23e170bf9b5fccb36122ffa4a3bdfba4b
DataTechNick <datatechnick@gmail.com>
2018-01-29 00:36:01 +0100

    socket.io

---
+++ pages/index.js
@@ -0,0 +1,37 @@
+// import Battle from '../components/battle';
+import io from 'socket.io-client';
+
+const socket = io.connect('localhost:3001');
+
+
+export default class ToDo extends React.Component {
+
+    constructor(props) {
+
+        super(props);
+        this.state = {
+            socketId: ""
+        }
+
+    }
+
+    componentWillMount() {
+
+        
+        socket.on('socketId', function (data) {
+            console.log(data);
+            this.setState({ socketId: data })
+        });
+
+    }
+
+    render = () => {
+        return (
+            <div>
+                <h1>{this.state.socketId}</h1>
+            </div>
+        )
+    }
+
+}
+
